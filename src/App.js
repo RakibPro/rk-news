5cbb75417e10d70f89cea586af8412d2b72b3bab
Rakib <rakibpro98@gmail.com>
2023-03-29 15:29:08 +0600

    route and page setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>RK_News</h1>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD10bh7pDnM9vGp9G8uqFjuPvmlmhvoba8",
+    authDomain: "rk-news-cdd11.firebaseapp.com",
+    projectId: "rk-news-cdd11",
+    storageBucket: "rk-news-cdd11.appspot.com",
+    messagingSenderId: "404400741702",
+    appId: "1:404400741702:web:9c5e821d70de8c67c79947"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
